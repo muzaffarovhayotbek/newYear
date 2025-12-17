d770103637cc58dac876d40db628858a848542d9
muzaffarovhayotbek <hayotbek0511@gmail.com>
2025-12-17 20:02:28 +0500

    new year project the end

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container main">
+      <h1 className="title">Happy New Year</h1>
+    </div>
+  );
+}
+
+export default App;
